527483b39a25727a41faf904fb7b59622ba7023e
pedroantonio120 <gozzi.pedro@escola.pr.gov.br>
2026-03-19 07:46:22 -0300

    update

---
+++ aulas/13022026.js
@@ -0,0 +1,29 @@
+let lucro = 30;
+let lucro2 = 10;
+ 
+function soma(a,b){
+     return a + b
+}
+soma(lucro,lucro2)
+  
+function resultadoDivisaoSocio(lucro,lucro2){
+  return soma (lucro,lucro2)/2
+}
+
+ function calculadora(ari,a,b){
+   switch(ari){
+     case '+':
+       resultado = a - b;
+       break;
+    case '-': 
+       resultado = a * b;
+       break;
+    case '/':
+       resultado = a / b;
+       break;
+       default:
+           resultado = 'Operador Invalido';
+       
+   }
+   return resultado
+ }   
